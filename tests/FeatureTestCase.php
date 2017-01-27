2a2cd65c35c052967684baeb3657abaf352819e7
Kontiki Velasco <evelasco@moli.mx>
2017-01-27 04:31:01 +0000

    Add feature test case

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ekontiki
+ * Date: 26/01/17
+ * Time: 22:25
+ */
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+
+}
